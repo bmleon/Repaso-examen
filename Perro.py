f5f607122244a298c2497814d7ca5bf855f36f4f
Belén María León <belenleon10.98@gmail.com>
2023-06-06 09:48:18 +0200

    Refactorizacion 1 Perro - Belen Leon

--- Perro.py
+++ Perro.py
@@ -2,11 +2,16 @@
 Clase Perro.
 Autor: Jaime Rabasco Ronda.
 Repaso examen
+Belén María León Fernández
 """
 class Perro:
 
     def ladrar(self):
-        print('Guau');
+        print(self.ladra());
+
+    def ladra(self):
+        return 'Guau'
+
 
 p = Perro();
 p.ladrar();
